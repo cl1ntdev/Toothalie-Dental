6f9cf0e740f94e19fa8dc703d1a628c6873b03c1
cl1ntdev <clintjayestrellanes17@gmail.com>
2026-01-17 13:14:42 +0800

    get services

---
+++ frontend/src/API/Authenticated/appointment/GetServices.ts
@@ -0,0 +1,19 @@
+
+export default async function getServices() {
+  const token = JSON.parse(localStorage.getItem('userInfo')).token;  
+  try {
+        const response = await fetch('/api/get-services',{
+          method: "GET",
+          headers: { 
+            "Content-Type": 'application/json', 
+            "Authorization" : `Bearer ${token}`
+          },
+        });
+        const data = await response.json()
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.error("Error fetching services:", error);
+        throw error;
+    }
+};
