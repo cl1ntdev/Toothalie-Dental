81cf4e10d82f24777e3039342af15c5ca2f02492
cl1ntdev <clintjayestrellanes17@gmail.com>
2026-04-20 20:17:36 +0800

    feat: centralized urls

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,32 @@
 import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite' // 1. Import loadEnv
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   resolve: {
-     alias: {
-       "@": path.resolve(__dirname, "./src"),
-     },
-   },
-   server: {
-     host:true,
-     allowedHosts: [
-       'uncomplaining-irritative-ranee.ngrok-free.dev',
-       'frontend-nine-zeta-40.vercel.app'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: true,
+      allowedHosts: [
+        'uncomplaining-irritative-ranee.ngrok-free.dev',
+        'frontend-nine-zeta-40.vercel.app'
       ],
-     port: 5173,
-     
-     
-     proxy: {
-         '/api': {
-           target: 'http://127.0.0.1:8000',
-           changeOrigin: true,
-           secure: false,
-         },
-       },
-   },
-   
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL, 
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 })
